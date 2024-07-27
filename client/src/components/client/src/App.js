549e4304aa2d0259f7c056d45a4fada7a1a94b44
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-27 15:18:30 -0700

    Create App.js

---
+++ client/src/components/client/src/App.js
@@ -0,0 +1,12 @@
+// client/src/App.js
+import React from 'react';
+import Chatbot from './components/Chatbot';
+
+const App = () => (
+  <div>
+    <h1>Chatbot</h1>
+    <Chatbot />
+  </div>
+);
+
+export default App;
