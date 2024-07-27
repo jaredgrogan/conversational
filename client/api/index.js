84aa74af452628af5ada19a2959e7b1f41706653
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-27 15:15:50 -0700

    Create index.js

---
+++ client/api/index.js
@@ -0,0 +1,17 @@
+// client/api/index.js
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
